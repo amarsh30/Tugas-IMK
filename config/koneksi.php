3662c2160f56929e2c1185d26148ab747e980158
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2023-01-30 09:01:10 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","db_sisekolah");
+$koneksi = mysqli_connect("103.93.56.27","tugasimk","satuduatiga","db_sisekolah");
  
 // Check connection
 if (mysqli_connect_errno()){
