026be4df219df4e35d430aff21e74c3101191f54
irfanca1 <irfanchoiruddin23@gmail.com>
2023-01-31 18:18:10 +0700

    laporan absensi dan laporan spp

--- admin/laporan/lapabsen_print.php
+++ admin/laporan/lapabsen_print.php
@@ -1,62 +1,51 @@
 <html>
-  <body>
-    <head>
-      <link rel="stylesheet" href="style.css" />
-    </head>
-    <table class="table responsive-3">
-      <h3 class="text-center">Laporan Absensi Siswa</h3>
-      <thead>
+
+<body>
+
+  <head>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <table class="table responsive-3">
+    <h3 class="text-center">Laporan Absensi Siswa</h3>
+    <thead>
+      <tr>
+        <th class="column-primary" data-header="User"><span>NIS</span></th>
+        <th>Nama</th>
+        <th>Kelas</th>
+        <th>Guru</th>
+        <th>Tanggal Absen</th>
+        <th>Absen</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      include '../../config/koneksi.php';
+      $sql = mysqli_query($koneksi, "SELECT tbl_absensi.tgl_absen, tbl_absensi.keterangan, tbl_siswa.nis,     tbl_siswa.nama, tbl_siswa.kelas, tbl_guru.nama as namaguru
+                    FROM tbl_absensi
+                    JOIN tbl_guru ON tbl_guru.id = tbl_absensi.id_guru
+                    JOIN tbl_siswa ON tbl_siswa.id = tbl_absensi.id_siswa");
+      foreach ($sql as $data) {
+      ?>
         <tr>
-          <th class="column-primary" data-header="User"><span>NIS</span></th>
-          <th>Nama</th>
-          <th>Kelas</th>
-          <th>Guru</th>
-          <th>Tanggal Absen</th>
-          <th>Absen</th>
+          <td data-header="NIM"><?= $data['nis']; ?></td>
+          <td data-header="Nama"><?= $data['nama']; ?></td>
+          <td data-header="Kelas"><?= $data['kelas']; ?></td>
+          <td data-header="Guru"><?= $data['namaguru']; ?></td>
+          <td data-header="TanggalAbsen" style="text-align: center;"><?= date('d F Y', strtotime($data['tgl_absen'])); ?></td>
+          <td data-header="Absen"><?php if ($data['keterangan'] == 'H') {
+                                    echo 'Hadir';
+                                  } elseif ($data['keterangan'] == 'S') {
+                                    echo 'Sakit';
+                                  } elseif ($data['keterangan'] == 'I') {
+                                    echo 'Izin';
+                                  } elseif ($data['keterangan'] == 'A') {
+                                    echo 'Alpa';
+                                  } ?>
+          </td>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Marcell</td>
-          <td data-header="Kelas">9-A</td>
-          <td data-header="Guru">Bu Anna</td>
-          <td data-header="TanggalAbsen">30-01-2023</td>
-          <td data-header="Absen">Hadir</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Amar</td>
-          <td data-header="Kelas">9-A</td>
-          <td data-header="Guru">Bu Anna</td>
-          <td data-header="TanggalAbsen">30-01-2023</td>
-          <td data-header="Absen">Hadir</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Irfan</td>
-          <td data-header="Kelas">9-A</td>
-          <td data-header="Guru">Bu Anna</td>
-          <td data-header="TanggalAbsen">30-01-2023</td>
-          <td data-header="Absen">Hadir</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Thoriq</td>
-          <td data-header="Kelas">9-A</td>
-          <td data-header="Guru">Bu Anna</td>
-          <td data-header="TanggalAbsen">30-01-2023</td>
-          <td data-header="Absen">Hadir</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Rasya</td>
-          <td data-header="Kelas">9-A</td>
-          <td data-header="Guru">Bu Anna</td>
-          <td data-header="TanggalAbsen">30-01-2023</td>
-          <td data-header="Absen">Hadir</td>
-        </tr>
-      </tbody>
-    </table>
-  </body>
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
+
 </html>
--- admin/laporan/lapspp_print.php
+++ admin/laporan/lapspp_print.php
@@ -1,50 +1,37 @@
 <html>
-  <body>
-    <head>
-      <link rel="stylesheet" href="style.css" />
-    </head>
-    <table class="table responsive-3">
-      <h3 class="text-center">Laporan Pembayaran SPP</h3>
-      <thead>
+
+<body>
+
+  <head>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <table class="table responsive-3">
+    <h3 class="text-center">Laporan Pembayaran SPP</h3>
+    <thead>
+      <tr>
+        <th class="column-primary" data-header="User"><span>NIS</span></th>
+        <th>Nama</th>
+        <th>Tanggal Pembayaran</th>
+        <th>Jumlah Pembayaran</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      include '../../config/koneksi.php';
+      $sql = mysqli_query($koneksi, "SELECT tbl_siswa.nis, tbl_siswa.nama, tbl_pembayaran_spp.tgl_pembayaran,       tbl_pembayaran_spp.total
+                    FROM tbl_pembayaran_spp
+                    JOIN tbl_siswa ON tbl_siswa.id = tbl_pembayaran_spp.id_siswa");
+      foreach ($sql as $data) {
+      ?>
         <tr>
-          <th class="column-primary" data-header="User"><span>NIS</span></th>
-          <th>Nama</th>
-          <th>Tanggal Pembayaran</th>
-          <th>Jumlah Pembayaran</th>
+          <td data-header="NIS"><?= $data['nis']; ?></td>
+          <td data-header="Nama"><?= $data['nama']; ?></td>
+          <td data-header="TanggalBayar" style="text-align: center;"><?= date('d F Y', strtotime($data['tgl_pembayaran'])); ?></td>
+          <td data-header="JumlahlBayar" style="text-align: right;"><?= "Rp. " . number_format($data['total'], 2, ',', '.'); ?></td>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td data-header="NIS">111111111</td>
-          <td data-header="Nama">Marcell</td>
-          <td data-header="TanggalBayar">30-01-2023</td>
-          <td data-header="JumlahlBayar">Rp. 250.000</td>
-        </tr>
-        <tr>
-          <td data-header="NIS">111111111</td>
-          <td data-header="Nama">Amar</td>
-          <td data-header="TanggalBayar">30-01-2023</td>
-          <td data-header="JumlahlBayar">Rp. 250.000</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Irfan</td>
-          <td data-header="TanggalBayar">30-01-2023</td>
-          <td data-header="JumlahlBayar">Rp. 250.000</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Thoriq</td>
-          <td data-header="TanggalBayar">30-01-2023</td>
-          <td data-header="JumlahlBayar">Rp. 250.000</td>
-        </tr>
-        <tr>
-          <td data-header="NIM">111111111</td>
-          <td data-header="Nama">Rasya</td>
-          <td data-header="TanggalBayar">30-01-2023</td>
-          <td data-header="JumlahlBayar">Rp. 250.000</td>
-        </tr>
-      </tbody>
-    </table>
-  </body>
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
+
 </html>
